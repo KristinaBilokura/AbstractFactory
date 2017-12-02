64a3cbeeeae00ae16f582728a7b500283b8d3d31
bilokura <kristinabilokura@gmail.com>
2017-12-02 17:45:39 +0200

    Done almost

--- src/main/java/AbstractFactory/BelgianEco.java
+++ src/main/java/AbstractFactory/BelgianEco.java
@@ -1,7 +1,20 @@
 package AbstractFactory;
 
+import AbstractFactory.Delivery.CourierService;
+import AbstractFactory.EcoProducts.EcoLifeCycle;
+import AbstractFactory.MarketingPackage.Advertising;
+import AbstractFactory.Production.Developing;
+
 public class BelgianEco {
     public static void main(String[] args) {
-        
+        LifeCycleOfProduct lifeCycle = new EcoLifeCycle();
+        Developing developer = lifeCycle.createProduction();
+        CourierService cservice = lifeCycle.createDelivery();
+        Advertising advertising=lifeCycle.create();
+
+        System.out.println("Show the whole LifeCycle");
+        developer.produce();
+        cservice.transfer();
+        advertising.write();
     }
 }
--- src/main/java/AbstractFactory/UAsimple.java
+++ src/main/java/AbstractFactory/UAsimple.java
@@ -1,7 +1,21 @@
 package AbstractFactory;
 
+import AbstractFactory.Delivery.CourierService;
+import AbstractFactory.MarketingPackage.Advertising;
+import AbstractFactory.Production.Developing;
+import AbstractFactory.SingleProducts.SinglePLifeCycle;
+
 public class UAsimple {
     public static void main(String[] args) {
+        LifeCycleOfProduct lifeCycleUA = new SinglePLifeCycle();
+        Developing production = lifeCycleUA.createProduction();
+        CourierService cservice = lifeCycleUA.createDelivery();
+        Advertising advertising=lifeCycleUA.create();
+
+        System.out.println("Show the whole LifeCycle");
+        production.produce();
+        cservice.transfer();
+        advertising.write();
 
     }
 }
